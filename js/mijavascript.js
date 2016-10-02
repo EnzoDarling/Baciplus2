b11b5ed1084b18319783234be8cf682d000a4cd3
EnzoDarling <crasshoverride07@gmail.com>
2016-10-02 14:44:10 -0300

    Agregado de tablas para cada formulario

--- js/mijavascript.js
+++ js/mijavascript.js
@@ -1,15 +1,16 @@
-$(document).ready(function() {
-
-  $(window).scroll(function () {
-      //if you hard code, then use console
-      //.log to determine when you want the 
-      //nav bar to stick.  
-      console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 20) {
-      $('.Menu').addClass('navbar-fixed');
-    }
-    if ($(window).scrollTop() < 20) {
-      $('.Menu').removeClass('navbar-fixed');
-    }
+$(document).ready(main);
+var contador = 1;
+function main(){
+  $('.menu-bar').click(function(){
+      // $('.Menu').toggle();
+      if(contador == 1){
+        $('.Menu').animate({
+          left: '0'
+        });
+      } else {
+         $('.Menu').animate({
+          left: '-100%'
+        });
+      }
   });
-});
+};
